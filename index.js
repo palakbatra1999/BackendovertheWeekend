5982dad3970175d1f4d439743dbde9c21329dd38
Palak Batra <palak99batra@gmail.com>
2024-11-02 14:06:59 +0530

    Commit message

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('Palak Batra reporting from Twitter')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
